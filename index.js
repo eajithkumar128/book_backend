7e2175736425712e13568dff992582d69176bdee
Ajithkumar <ajithkumar@N10382.local>
2021-01-23 19:16:40 +0530

    add request

--- index.js
+++ index.js
@@ -10,28 +10,32 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.post("/paymentGateway", async (req, res) => {
-	Instamojo.setKeys(process.env.API_KEY, process.env.API_AUTH);
+  Instamojo.setKeys(process.env.API_KEY, process.env.API_AUTH);
 
-	let data = {};
-	Instamojo.isSandboxMode(true);
+  let data = {};
+  Instamojo.isSandboxMode(true);
 
-	data.purpose = req.body.purpose;
-	data.amount = req.body.amount;
-	data.buyer_name = req.body.buyer_name;
-	data.redirect_url = req.body.redirect_url;
-	data.email = req.body.email;
-	data.phone = req.body.phone;
-	data.send_email = false;
-	data.webhook = "http://www.example.com/webhook/";
-	data.send_sms = false;
-	data.allow_repeated_payments = false;
+  data.purpose = req.body.purpose;
+  data.amount = req.body.amount;
+  data.buyer_name = req.body.buyer_name;
+  data.redirect_url = req.body.redirect_url;
+  data.email = req.body.email;
+  data.phone = req.body.phone;
+  data.send_email = false;
+  data.webhook = "http://www.example.com/webhook/";
+  data.send_sms = false;
+  data.allow_repeated_payments = false;
 
-	const paymentData = Instamojo.PaymentData(data);
-	const response = await Instamojo.createNewPaymentRequest(paymentData);
-	const redirectUrl = response.payment_request.longurl;
-	res.status(200).json(redirectUrl);
+  const paymentData = Instamojo.PaymentData(data);
+  const response = await Instamojo.createNewPaymentRequest(paymentData);
+  const redirectUrl = response.payment_request.longurl;
+  res.status(200).json(redirectUrl);
+});
+
+app.get("/", (req, res) => {
+  res.send("heloo work");
 });
 
 app.listen(process.env.PORT, () => {
-	console.log("Server started");
+  console.log("Server started");
 });
